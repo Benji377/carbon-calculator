4e704e2ed2de6786a4aaafb5c0b8e3dc1acecde4
Benjamin Demetz <benjamin.demetz@student.tugraz.at>
2026-03-19 16:05:42 +0100

    Cleanup
    Cleanup before migration to new framework

--- src/app/shared/diagram-data.interface.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Interface for the data that is used to draw the diagram.
- */
-export interface DiagramData {
-  /** name of the diagram */
-	name: string,
-  /** value of the diagram */
-	value: number
-}
